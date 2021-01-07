11e099a0c1aedea74e9d839668157433dd108056
Bilal AMRI <iambilalamri@gmail.com>
2021-01-07 19:27:14 +0100

    [feat-generics]-work on generics

--- src/amrib/cleancode/generics/Main.java
+++ src/amrib/cleancode/generics/Main.java
@@ -7,19 +7,32 @@ public class Main {
 		List list = new List();
 		list.add(5);
 		list.add(7);
-		
+
 		System.out.println("*****************");
 		GenericList<Integer> intList = new GenericList<Integer>();
 		intList.add(5);
 		intList.add(4);
 		Integer item1 = intList.get(1);
 		System.out.println(item1);
-		
+
 		System.out.println("*****************");
-		GenericList<String> stringList = new GenericList<String>();
-		stringList.add("aaa");
-		stringList.add("bbb");
+		GenericList<Double> stringList = new GenericList<Double>();
+		stringList.add(12.3);
+		stringList.add(5.3);
 		System.out.println(stringList.get(0));
+		
+		System.out.println("******************");
+		GenericList<User> userList = new GenericList<User>();
+		
+		System.out.println("******************");
+		User user1 = new User(10);
+		User user2 = new User(20);
+		if (user1.compareTo(user2) > 0)
+			System.out.println("user1 > user2");
+		else if (user1.compareTo(user2) < 0)
+			System.out.println("user1 < user2");
+		else
+			System.out.println("user1 == user2");
 	}
 
 }
