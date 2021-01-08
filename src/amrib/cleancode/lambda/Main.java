354afd907d84dc3d3ecd8efa2eda10868b38525d
Bilal AMRI <iambilalamri@gmail.com>
2021-01-08 23:16:25 +0100

    [feat-generics]-work on lambdas and add comments on readme

--- src/amrib/cleancode/lambda/Main.java
+++ src/amrib/cleancode/lambda/Main.java
@@ -1,6 +1,13 @@
 package amrib.cleancode.lambda;
 
+import java.util.Collection;
+import java.util.Collections;
+
 public class Main {
+
+	public Main(String message) {
+		// TODO Auto-generated constructor stub
+	}
 
 	public static void show() {
 		// Call implementation of interface
@@ -16,8 +23,28 @@ public class Main {
 		});
 
 		// use lambda function
-		greet(message -> System.out.println(message));
 		Printer p = message -> System.out.println(message);
+		System.out.println("***********************");
+
+		// ***********************************
+		// greet(message -> System.out.println(message));
+		greet(System.out::println);
+		greet(Main::print);
+
+		// ***********************************
+		Main demo = new Main("");
+		// greet(message -> demo.traitment(message));
+		greet(demo::traitment);
+
+		// ***********************************
+		// greet(message -> new Main(message));
+		greet(Main::new);
+	}
+
+	public static void print(String message) {
+	}
+
+	public void traitment(String type) {
 	}
 
 	public static void greet(Printer printer) {
