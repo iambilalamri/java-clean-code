49be8f2de288df97b39b771cf4e1f31118df29bc
Bilal AMRI <iambilalamri@gmail.com>
2021-01-13 23:38:12 +0100

    [feat-streams]- working on threads

--- src/amrib/cleancode/thread/DownloadFileTask.java
+++ src/amrib/cleancode/thread/DownloadFileTask.java
@@ -2,16 +2,31 @@ package amrib.cleancode.thread;
 
 public class DownloadFileTask implements Runnable {
 
+	// @Override
+	// public void run() {
+	// // TODO Auto-generated method stub
+	// System.out.println("Downloading a FILE: " +
+	// Thread.currentThread().getName());
+	//
+	// try {
+	// Thread.sleep(5000);
+	// } catch (InterruptedException e) {
+	// // TODO Auto-generated catch block
+	// e.printStackTrace();
+	// }
+	//
+	// System.out.println("Download complete" + Thread.currentThread().getName());
+	// }
+
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
 		System.out.println("Downloading a FILE: " + Thread.currentThread().getName());
 
-		try {
-			Thread.sleep(5000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		for (int i = 0; i < Integer.MAX_VALUE; i++) {
+			if (Thread.currentThread().isInterrupted())
+				return;
+			System.out.println("Downloading byte " + i);
 		}
 
 		System.out.println("Download complete" + Thread.currentThread().getName());
--- src/amrib/cleancode/thread/ThreadMain.java
+++ src/amrib/cleancode/thread/ThreadMain.java
@@ -3,12 +3,23 @@ package amrib.cleancode.thread;
 public class ThreadMain {
 
 	public static void main(String[] args) {
-		startThread();
+		// startThread();
 		// System.out.println(Thread.activeCount());
 		// System.out.println(Runtime.getRuntime().availableProcessors());
+		interruptThread();
 	}
 
 	public static void interruptThread() {
+		Thread thread = new Thread(new DownloadFileTask());
+		thread.start();
+
+		try {
+			thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		thread.interrupt();
 	}
 
 	public static void joinThread() {
